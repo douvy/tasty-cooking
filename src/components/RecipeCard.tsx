e8fec3f0aafe0e70ab1dd30e805d4b4d8da4f130
douvy <rjd5146@gmail.com>
2025-05-31 23:03:58 -0400

    Made card hover on homepage more subtle and elegant

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -43,7 +43,7 @@ const RecipeCard: React.FC<RecipeCardProps> = ({
   return (
     <Link 
       href={`/${recipe.link}`} 
-      className={`bg-dark-gray border-divider-all hover:border-light-grayish-orange-all rounded-3xl overflow-hidden shadow-lg relative h-48 sm:h-64 block group transition-all duration-500 transform hover:scale-[1.01] ${className}`}
+      className={`bg-dark-gray border-divider-all hover:border-light-grayish-orange-all rounded-3xl overflow-hidden shadow-lg relative h-48 sm:h-64 block group transition-all duration-500 transform ${className}`}
       data-tags={recipe.tags.join(' ')}
       style={style}
     >
@@ -54,7 +54,7 @@ const RecipeCard: React.FC<RecipeCardProps> = ({
             alt={recipe.title}
             fill
             sizes="(max-width: 640px) 50vw, (max-width: 1024px) 33vw, 25vw"
-            className="object-cover transition-all duration-500 group-hover:opacity-90"
+            className="object-cover transition-all duration-500 group-hover:opacity-70"
             priority={true}
             placeholder="empty"
             onLoad={() => setIsLoaded(true)}
@@ -73,7 +73,7 @@ const RecipeCard: React.FC<RecipeCardProps> = ({
         )}
       </div>
       <div className="absolute bottom-0 left-0 w-full text-center py-4 pb-0">
-        <div className="w-11/12 mx-auto border-grayish-orange-all py-2 bg-[#e2d7a0] rounded-3xl transition-colors duration-500 group-hover:bg-[#eadfae]">
+        <div className="w-11/12 mx-auto border-grayish-orange-all py-2 bg-[#e2d7a0] rounded-3xl transition-colors duration-500 group-hover:bg-[#eee3b7]">
           <div className="font-bold text-[#141416] text-xs uppercase pt-2 pb-2 recipe-title leading-2 sm:leading-5">
             {getDisplayTitle(recipe.link, recipe.title)}
           </div>
